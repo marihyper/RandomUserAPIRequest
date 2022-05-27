6271882e6f63c8fbb16b6204e9b1a9567478eaf7
Mariana <mariana@wicked.art.br>
2022-05-26 21:26:32 -0300

    added connectToApi
    Signed-off-by: Mariana <mariana@wicked.art.br>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,10 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -5,30 +12,43 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class Main {
+    //response
+    private static String response = connectToApi();
 
-    //api url
-    private static final String POST_API_URL = "https://randomuser.me/api/?results=10&inc=gender,name,email,id&noinfo";
-    
-    public static void main(String[] args) throws  IOException, InterruptedException{
-        
+    public static String connectToApi() {
         //client
         HttpClient client = HttpClient.newHttpClient();
-        
-        //request
+        //api url with field inclusion specification
+        String POST_API_URL = "https://randomuser.me/api/?results=10&inc=gender,name,email,id&noinfo";
+        //send async request
         HttpRequest request = HttpRequest.newBuilder()
                 .GET()
                 .header("accept", "application/json")
                 .uri(URI.create(POST_API_URL))
                 .build();
-        
-        //send async request
-        String response = client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+        response = client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body)
                 .join();
-        System.out.println(response);
-        
-        //send sync request
-//        client.send(request, HttpResponse.BodyHandlers.ofString());
-//        System.out.println(response.body());
+        return response;
+    }
+
+    public static void main(String[] args) throws IOException, ParseException {
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(response);
+        JSONArray jsonArray = (JSONArray) jsonObject.get("results");
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode node = objectMapper.readTree(String.valueOf(jsonArray));
+        if (node.isArray()) {
+            for (JsonNode jsonNode : node) {
+                User user = objectMapper.treeToValue(jsonNode, User.class);
+                System.out.println("Gender : " + user.getGender());
+                Name name = user.getName();
+                System.out.println("Name : " + name.getTitle() + " " + name.getFirst() + " " + name.getLast());
+                System.out.println("Email : " + user.getEmail());
+                Id id = user.getId();
+                System.out.println("Id : name : " + id.getName() + ", value: " + id.getValue());
+                System.out.println();
+            }
+        }
     }
 }
